0840f8a6af487d8a05c323bd1b3b0b5a5c187478
MaksumulEmon <maksumulemon300@gmail.com>
2026-04-14 12:37:40 +0600

    Friens components done

--- src/Components/homepageComponents/YourFriends.jsx
+++ src/Components/homepageComponents/YourFriends.jsx
@@ -3,14 +3,14 @@ import React, { use, useEffect, useState } from 'react';
 const YourFriends = () => {
 
 
-    const [apps, setApps] = useState([]);
+    const [friends, setFriends] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             const res = await fetch("/data.json");
             const data = await res.json();
             console.log(data)
-            setApps(data);
+            setFriends(data);
 
 
             // setTimeout(() => {
@@ -26,7 +26,37 @@ const YourFriends = () => {
     return (
         <div className='bg-[#F8FAFC]'>
             <div className='max-w-7xl mx-auto'>
-                <p className='text-2xl  font-semibold'>Your Friends</p>
+                <p className='text-2xl  font-semibold pt-5 lg:pt-16 pb-2 m-4'>Your Friends</p>
+
+
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  lg:gap-4 lg:pb-10 '>
+
+                    {friends.map((friend, index) => {
+                        return <div key={index} className='rounded-2xl hover:shadow-2xl transition '>
+                            <div className='text-center p-6  rounded-xl  bg-white shadow-[0_3px_6px_0_rgba(0,0,0,0.08)] space-y-4 m-4 lg:m-0'>
+
+                                <div className='flex justify-center items-center '>
+                                    <img src={friend.picture} alt="" className='w-20 h-20 rounded-full ' />
+                                </div>
+                                <p className='text-xl text-[#1F2937] font-semibold'>{friend.name}</p>
+                                <p className='text-[#64748B] '>{friend.days_since_contact}day ago</p>
+                                <p className='text-[#64748B]'>{friend.tags}</p>
+                                {/* <p className={`text-white ${friend.status === "almost due" ? "bg-[#EFAD44]" : "bg-red-500"}`}>
+                                    {friend.status}
+                                </p> */}
+
+                                <p className={`text-white py-2 px-3 rounded-full inline-block
+                                ${friend.status === "almost due" && "bg-[#EFAD44]"}
+                                ${friend.status === "overdue" && "bg-[#EF4444]"} 
+                                ${friend.status === "on-track" && "bg-[#244D3F]"}`}
+                                >{friend.status}</p>
+                            </div>
+                        </div>
+                    })}
+
+                </div>
+
+
 
 
 
