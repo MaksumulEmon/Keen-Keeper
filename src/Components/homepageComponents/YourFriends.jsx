07f1d925f34a944ce26862801d1c16a87c8342d7
MaksumulEmon <maksumulemon300@gmail.com>
2026-04-14 11:20:41 +0600

    Adddddddddd

--- src/Components/homepageComponents/YourFriends.jsx
+++ src/Components/homepageComponents/YourFriends.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { use, useEffect, useState } from 'react';
 
 const YourFriends = () => {
+
+
+    const [apps, setApps] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch("/data.json");
+            const data = await res.json();
+            console.log(data)
+            setApps(data);
+
+
+            // setTimeout(() => {
+            //     setApps(data);
+            //     setLoading(false)
+            // }, 1000);
+        };
+
+        fetchData();
+    }, []);
+
+
     return (
         <div className='bg-[#F8FAFC]'>
             <div className='max-w-7xl mx-auto'>
                 <p className='text-2xl  font-semibold'>Your Friends</p>
+
+
+
 
             </div>
         </div>
